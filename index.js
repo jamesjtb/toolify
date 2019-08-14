886f0ee6a3d677b73147add29e6ce93ac7c30df7
James Johnes <james@allpointpos.com>
2019-08-14 11:29:40 -0400

    compareObjects to areObjects equal, add exclusion

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fs              = require('fs');              // File system access
+const fs = require('fs');              // File system access
 
 // Convert an array to an object where the key is the specified identifier. This key must exist and have a unique value in every object within the input array.
 // Usage: objectify(someArray, 'SomeKey')
@@ -52,32 +52,32 @@ module.exports.denilify = function (input, attributeNode = 'attr') {
 // Compare the properties AND values of two objects; if they are all equalivalent, then return true; otherwise, return false. Use truthiness if the third argument, truthy, is true.
 // The fourth argument, "depth", is used by the function itself when it is called recursively. 
 // "There is a difference between knowing the path and walking the path." -- Morpheus
-module.exports.compareObjects = function (object1, object2, options = { truthy: false, unidirectional: false, depth: 0 }) {
+module.exports.areObjectsEqual = function (object1, object2, { truthy = false, unidirectional = false, depth = 0, exclude = [] }) {
   try {
-    if (options.truthy == undefined) options.truthy = false;
-    if (options.unidirectional == undefined) options.unidirectional = false;
+    if (truthy == undefined) truthy = false;
+    if (unidirectional == undefined) unidirectional = false;
     if ((object1 == null && object2 != null) || (object1 != null && object2 == null)) return false;
     if (object1 == null && object2 == null) return true;
     // Guard statement for the possibility of an infinite recursion
     let maxDepth = 5000;
-    if (options.depth > maxDepth) throw new Error(`The compareObjects function has exceeded the predefined maximum object depth of ${maxDepth}. Check your objects for any self references.`);
+    if (depth > maxDepth) throw new Error(`The compareObjects function has exceeded the predefined maximum object depth of ${maxDepth}. Check your objects for any self references.`);
 
     // Main Logic
 
-    if (!options.unidirectional) { // if the comparison is not unidirectional...
+    if (!unidirectional) { // if the comparison is not unidirectional...
       // A key check for the second object against the first should be all that is needed to ensure that the comparison is bidirectional.
       for (let key in object2) {
-        if (object1[key] === undefined && object2[key] !== undefined) return false; // sometimes a key is defined as undefined. The second comparison ensures that if object1[key] is undefined, that object2[key] is not also undefined.
+        if (object1[key] === undefined && object2[key] !== undefined && !exclude.includes(key)) return false; // sometimes a key is defined as undefined. The second comparison ensures that if object1[key] is undefined, that object2[key] is not also undefined.
       }
     }
     for (let property in object1) {
-      if (typeof object1[property] === 'object') {
-        if (module.exports.compareObjects(object1[property], object2[property], { truthy: options.truthy, unidirectional: options.truthy, depth: options.depth + 1 }) === false) return false;
+      if (typeof object1[property] === 'object' && !exclude.includes(property)) {
+        if (module.exports.compareObjects(object1[property], object2[property], { truthy: truthy, unidirectional: truthy, depth: depth + 1 }) === false) return false;
       } else {
-        if (options.truthy === true) { // Use "truthy" comparison if truthy is set to true.
-          if (object1[property] != object2[property]) return false;
+        if (truthy === true) { // Use "truthy" comparison if truthy is set to true.
+          if (object1[property] != object2[property] && !exclude.includes(property)) return false;
         } else {
-          if (object1[property] !== object2[property]) return false;
+          if (object1[property] !== object2[property] && !exclude.includes(property)) return false;
         }
       }
     }
@@ -98,60 +98,60 @@ module.exports.asynctimeout = ms => new Promise(res => setTimeout(res, ms));
 //#### Remove all of the json objects that are null ####
 //“Getting rid of dragons is not at all in my line, but I will do my best to think about it." --Bilbo Baggins
 module.exports.removeNull = function (obj) {
-  Object.keys(obj).forEach(function(key) {
-    var self =  module.exports;
-   (obj[key] && typeof obj[key] === 'object') && self.removeNull(obj[key]) ||
-   (obj[key] === null) && delete obj[key]
+  Object.keys(obj).forEach(function (key) {
+    var self = module.exports;
+    (obj[key] && typeof obj[key] === 'object') && self.removeNull(obj[key]) ||
+      (obj[key] === null) && delete obj[key]
   });
- }
+}
 
- //#### Find a json object if it exists: where (the object), what (the search term, a RegEx or plain text), searchMethod search in the key, data, or both ####
- // This returns an array of objects containing key PATH (as a string) and the data at that path in the object
-   //uesage results=deepObjectSearch(objectIWantToSearch,/^green/,'key'); //search for all keys that start with green.
-   //uesage results=deepObjectSearch(objectIWantToSearch,'retail','data'); //search for all object that have 'retail' as data.
- //"There is nothing like looking, if you want to find something." --Thorin
+//#### Find a json object if it exists: where (the object), what (the search term, a RegEx or plain text), searchMethod search in the key, data, or both ####
+// This returns an array of objects containing key PATH (as a string) and the data at that path in the object
+//uesage results=deepObjectSearch(objectIWantToSearch,/^green/,'key'); //search for all keys that start with green.
+//uesage results=deepObjectSearch(objectIWantToSearch,'retail','data'); //search for all object that have 'retail' as data.
+//"There is nothing like looking, if you want to find something." --Thorin
 module.exports.deepObjectSearch = function (where, what, searchMethod) {
   var res = [];  //the output array
 
- // Choose a search method (RegEx or plain text)
+  // Choose a search method (RegEx or plain text)
   if (Object.prototype.toString.call(what).match(/^\[object\s(.*)\]$/)[1].toLowerCase() === "regexp") {  //its regex
     var match = function match(obj, key) {
-      switch(searchMethod){
+      switch (searchMethod) {
         case 'key':
-         return what.test(key);                            //this searches in keys
-         break;
+          return what.test(key);                            //this searches in keys
+          break;
         case 'data':
-         return what.test(obj[key]);                       //this searches in data
-         break
+          return what.test(obj[key]);                       //this searches in data
+          break
         default:
-         return what.test(obj[key]) || what.test(key);     //this searches both in data and keys
+          return what.test(obj[key]) || what.test(key);     //this searches both in data and keys
       }
     }
   } else {  //its plain text
     var match = function match(obj, key) {
-      switch(searchMethod){
+      switch (searchMethod) {
         case 'key':
-         return key == what;                            //this searches in keys
-         break;
+          return key == what;                            //this searches in keys
+          break;
         case 'data':
-         return obj[key] == what;                       //this searches in data
-         break
+          return obj[key] == what;                       //this searches in data
+          break
         default:
-         return obj[key] == what || key == what;       //this searches both in data and keys
+          return obj[key] == what || key == what;       //this searches both in data and keys
       }
     }
   }
- //Set up the actual search
+  //Set up the actual search
   function innerSearch(o, v) {
-   //The recursive search function
+    //The recursive search function
     var search = function search(obj, pattern, prefix) {
       prefix = prefix || '';
-      return function(a) {
-        if (typeof obj[a] === "object" && obj[a] !== null && !(match(obj, a)) ) {
+      return function (a) {
+        if (typeof obj[a] === "object" && obj[a] !== null && !(match(obj, a))) {
           Object.keys(obj[a]).forEach(search(obj[a], pattern, /\d+/.test(a) ? prefix + '[' + a + ']' : (prefix !== '' ? prefix + '.' : '') + a));
         } else {
           if (match(obj, a)) {
-            res.push({[prefix +'.'+a]:obj[a]}); //it matches! Add it to the list.
+            res.push({ [prefix + '.' + a]: obj[a] }); //it matches! Add it to the list.
           }
         }
       };
@@ -160,46 +160,46 @@ module.exports.deepObjectSearch = function (where, what, searchMethod) {
   }
 
   innerSearch(where, what);
-  if (res.length < 1) {return;} //return nothing if the array is empty instead of empty array
-  return(res); //return the array of everything that matched
+  if (res.length < 1) { return; } //return nothing if the array is empty instead of empty array
+  return (res); //return the array of everything that matched
 }
 
 //#### Safely get the data from a path in an object ####
 // useage: deepFetch('ApiDocument.Response.Styles[0].EChannels[1].Name',dataObject);
-  //"There you are, Thief in the Shadows." --Smaug
-module.exports.deepFetch = function (path,obj){
-  if (! path ) {return;}
+//"There you are, Thief in the Shadows." --Smaug
+module.exports.deepFetch = function (path, obj) {
+  if (!path) { return; }
   try {
     let segments = path.split('.');
     for (let i = 0; i < segments.length; ++i) {
       let segment = segments[i];
-      let index=segments[i].split('[')[1];
+      let index = segments[i].split('[')[1];
       if (index) {
         index = index.split(']')[0];
         segment = segment.split('[')[0];
         obj = obj[segment][index];
-      } else { obj = obj[ segment ] }
+      } else { obj = obj[segment] }
     }
-  } catch (e) {return;};
-  if (typeof obj !== 'undefined') {return obj}
+  } catch (e) { return; };
+  if (typeof obj !== 'undefined') { return obj }
 }
 
 //#### Safely check if a path exists inside an object ####
 //  useage if ( deepVerify('ApiDocument.Response.Styles',dataObject) ) { do.stuff() }
 //"Few can foresee whither their road will lead them, till they come to its end." --Legolas
-module.exports.deepVerify = function (path,obj){
+module.exports.deepVerify = function (path, obj) {
   try {
     let segments = path.split('.');
     for (let i = 0; i < segments.length; ++i) {
       let segment = segments[i];
-      let index=segments[i].split('[')[1];
+      let index = segments[i].split('[')[1];
       if (index) {
         index = index.split(']')[0];
         segment = segment.split('[')[0];
         obj = obj[segment][index];
-      } else { obj = obj[ segment ] }
+      } else { obj = obj[segment] }
     }
-    if (typeof obj !== 'undefined') {return true;} else {return false;}
+    if (typeof obj !== 'undefined') { return true; } else { return false; }
   } catch (e) {
     return false;
   }
@@ -208,90 +208,90 @@ module.exports.deepVerify = function (path,obj){
 //#### Function to return an array of lines containing all of the paths in an object and a guess at their data types ####
 // useage: let arrayOfLines = objectMap(dataObject,'MyPrefix');
 //"The world is not in your books and maps, it’s out there." – Gandalf
-module.exports.objectMap = function (obj,prefix=''){
-  let output=[];
- 
-     const lineify = function (obj,prefix='',wasArray=false){
-       if ( typeof obj != 'object' ) {return}
- 
-       let lines=Object.keys(obj);
- 
-       lines.forEach(line=>{
-         let dtype = typeof obj[line];
-         let partOfArray = ( obj[ line ] instanceof Array );
- 
-         if (dtype != 'object') { output.push(`${prefix}.${line} :: ${dtype}`) }
-         else {
-           if ( partOfArray ) { lineify( obj[ line ],`${prefix}.${line}`,partOfArray) }
-           else {
-             if (wasArray) {lineify( obj[ line ],`${prefix}[${line}]`)}
-             else {lineify( obj[ line ],prefix+'.'+line)}
-           }
-         }
-       });
-     }
-  
-   lineify(obj,prefix);
-   return output;
- }
+module.exports.objectMap = function (obj, prefix = '') {
+  let output = [];
+
+  const lineify = function (obj, prefix = '', wasArray = false) {
+    if (typeof obj != 'object') { return }
+
+    let lines = Object.keys(obj);
+
+    lines.forEach(line => {
+      let dtype = typeof obj[line];
+      let partOfArray = (obj[line] instanceof Array);
+
+      if (dtype != 'object') { output.push(`${prefix}.${line} :: ${dtype}`) }
+      else {
+        if (partOfArray) { lineify(obj[line], `${prefix}.${line}`, partOfArray) }
+        else {
+          if (wasArray) { lineify(obj[line], `${prefix}[${line}]`) }
+          else { lineify(obj[line], prefix + '.' + line) }
+        }
+      }
+    });
+  }
+
+  lineify(obj, prefix);
+  return output;
+}
 
 //#### Function to determine if your input is an object (but not an array) ####
 // uesage: if (isObject(input)) {do stuff;}
 // "What are we holding onto, Sam?" --Frodo
- module.exports.isObject = val => typeof val === 'object' && !Array.isArray(val);
+module.exports.isObject = val => typeof val === 'object' && !Array.isArray(val);
 
 
- //#### Function to remove a list of unwanted characters from input text
-   //"It is not our part to master all the tides of the world, but to do what is in us 
-   //for the succor of those years wherein we are set, uprooting the evil in the fields 
-   //that we know, so that those who live after may have clean earth to till. — Gandalf"
+//#### Function to remove a list of unwanted characters from input text
+//"It is not our part to master all the tides of the world, but to do what is in us 
+//for the succor of those years wherein we are set, uprooting the evil in the fields 
+//that we know, so that those who live after may have clean earth to till. — Gandalf"
 //usage: result = removeBogusChars('Input String');
-module.exports.removeBogusChars = function (input,bogusList){
-  if (typeof input !== 'string') {return input;}
-  if (typeof bogusList == 'undefined') { bogusList = ['\u200B','\u200C','\u200D','\uFEFF',]} //use builtin
-   else {bogusList = module.exports.arrayify(bogusList)}
-  bogusList.forEach(c=>{ input = input.replace(c, '') });
+module.exports.removeBogusChars = function (input, bogusList) {
+  if (typeof input !== 'string') { return input; }
+  if (typeof bogusList == 'undefined') { bogusList = ['\u200B', '\u200C', '\u200D', '\uFEFF',] } //use builtin
+  else { bogusList = module.exports.arrayify(bogusList) }
+  bogusList.forEach(c => { input = input.replace(c, '') });
   return input;
 }
 
 
 //#### Function to take an array of key names and an array of data values and return an object
-  //"Real names tell you the story of the things they belong to" --Treebeard
+//"Real names tell you the story of the things they belong to" --Treebeard
 //usage: result = arraysToObject([keynames], [values]);
-module.exports.arraysToObject = function (keys,values){
-  keys   = module.exports.arrayify(keys);
-  values = module.exports.arrayify(values);            
-  let output={};
-  for (let i=0; i < keys.length; i++){
-    if (typeof values[i] !== 'undefined') {output[keys[i]]=values[i];}
-     else {output[keys[i]]=null}
+module.exports.arraysToObject = function (keys, values) {
+  keys = module.exports.arrayify(keys);
+  values = module.exports.arrayify(values);
+  let output = {};
+  for (let i = 0; i < keys.length; i++) {
+    if (typeof values[i] !== 'undefined') { output[keys[i]] = values[i]; }
+    else { output[keys[i]] = null }
   }
   return output;
 }
 
 //#### Find all directories recursively ####
-  //"We must take a hard road, a road unforeseen. There lies our hope, if hope it be." --Elrond
+//"We must take a hard road, a road unforeseen. There lies our hope, if hope it be." --Elrond
 //useage : listOfPaths = findDirs('startLookingHere');
-  module.exports.findDirs = function (dir, filelist) {
+module.exports.findDirs = function (dir, filelist) {
   var fs = fs || require('fs'),
-  files = fs.readdirSync(dir);
+    files = fs.readdirSync(dir);
   filelist = filelist || [];
-  files.forEach(function(file) {
-      if (fs.statSync(dir + '/' + file).isDirectory()) {
-          filelist.push(dir + file);
-          filelist = module.exports.findDirs(dir + file + '/', filelist);
-      }
+  files.forEach(function (file) {
+    if (fs.statSync(dir + '/' + file).isDirectory()) {
+      filelist.push(dir + file);
+      filelist = module.exports.findDirs(dir + file + '/', filelist);
+    }
   });
   return filelist;
 };
 
 //#### Check if a directory has a given file in it ####
-  //"I don’t know, and I would rather not guess." --Frodo
+//"I don’t know, and I would rather not guess." --Frodo
 //useage : if ( containsFile('directoryToSearch','fileName) ) { console.log('Found it') }
-  module.exports.containsFile = function (inPath,inFile){
-    let output = false;
-    fs.readdirSync(inPath).forEach(file => {
-      if (file === inFile ){ output = true;}
-    });
-    return output;
-  }
+module.exports.containsFile = function (inPath, inFile) {
+  let output = false;
+  fs.readdirSync(inPath).forEach(file => {
+    if (file === inFile) { output = true; }
+  });
+  return output;
+}
